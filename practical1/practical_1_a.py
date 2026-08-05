5e705c8e33084210c70ef03a5a2530177ebd7ad4
Anurag Puthiyaveetil Othayoth <anuragpo393@gmail.com>
2026-08-05 11:15:39 +0530

    Add basic arithmetic operations in practical_1_a.py

---
+++ practical1/practical_1_a.py
@@ -0,0 +1,6 @@
+a, b = int(input("A : ")), int(input("B : "))
+
+print("Sum:", a + b )
+print("Difference",  a - b )
+print("Product", a * b )
+print("Division",  a / b if b != 0 else "Cannot divide by 0")
