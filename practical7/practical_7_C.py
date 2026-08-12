08ecff7c090ee2fcf4578a9eebf2089e6536054d
Anurag Puthiyaveetil Othayoth <anuragpo393@gmail.com>
2026-08-12 22:45:46 +0530

    Simplify division operation input
    Refactor input and calculation into a single line.

--- practical7/practical_7_C.py
+++ practical7/practical_7_C.py
@@ -1,9 +1,11 @@
 try:
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
 
-    result = a / b
-    print("Result:", result)
+    print("Result: ", int(input("Enter first number: ")) / int(input("Enter second number: ")))
+    # a = int(input("Enter first number: "))
+    # b = int(input("Enter second number: "))
+
+    # result = a / b
+    # print("Result:", result)
 
 except ValueError:
     print("Error: Please enter valid integers.")
